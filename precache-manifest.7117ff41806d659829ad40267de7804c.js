b6c8320d242598f1c7f308c3b6e1dbe2be2941f3
Tyler Maxwell <tylermaxwell661@gmail.com>
2019-05-29 15:40:53 -0700

    Updates

---
+++ precache-manifest.7117ff41806d659829ad40267de7804c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae96eaa953f7b514aeb35102ccf5167d",
+    "url": "/dice-app/index.html"
+  },
+  {
+    "revision": "2ffe142c83936a174a35",
+    "url": "/dice-app/static/css/main.93954237.chunk.css"
+  },
+  {
+    "revision": "9eee67cf782a38d35aeb",
+    "url": "/dice-app/static/js/2.d6a534e5.chunk.js"
+  },
+  {
+    "revision": "2ffe142c83936a174a35",
+    "url": "/dice-app/static/js/main.fc90fe42.chunk.js"
+  },
+  {
+    "revision": "15f368191d94c6e2b1c5",
+    "url": "/dice-app/static/js/runtime~main.02aad080.js"
+  }
+]);
