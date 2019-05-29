ae0e71da1f86f8685aa15ef7b9e24ad51424da4c
Tyler Maxwell <tylermaxwell661@gmail.com>
2019-05-29 15:37:25 -0700

    init commit

---
+++ src/Die.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './Die.css';
+
+export default class Die extends Component {
+  render() {
+    return (
+      <div>
+        <i
+          className={`Die fas fa-dice-${this.props.face} ${
+            this.props.rolling ? 'Rolling' : ''
+          }`}
+        />
+      </div>
+    );
+  }
+}
